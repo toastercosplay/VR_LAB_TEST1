813a242a8d51ca279e429768083f522edf3bd660
toastercosplay <andrewdouglas2005@gmail.com>
2026-01-21 14:08:57 -0800

    vr setup testing

--- Assets/SampleSine.cs
+++ Assets/SampleSine.cs
@@ -6,6 +6,9 @@ public class SampleSine : MonoBehaviour
     bool timeEnabled = true;
     public float frequency = 1f;
     public float amplitude = 1f;
+    [SerializeField] float maximum = 4;
+    //[SerializeField] float startTime = 0;
+
 
     //Transform zero;
     Transform self;
@@ -13,6 +16,7 @@ public class SampleSine : MonoBehaviour
     void Start()
     {
         self = GetComponent<Transform>();
+        timeElapsed = ((self.position.x / maximum) / frequency) + ((self.position.z / maximum) / frequency); 
     }
 
     void Update()
@@ -21,10 +25,11 @@ public class SampleSine : MonoBehaviour
         {
             return;
         }
+
         timeElapsed += Time.deltaTime;
         float y = amplitude * Mathf.Sin(2 * Mathf.PI * frequency * timeElapsed);
-        float x = timeElapsed;
-        self.position = new Vector3(x, y, self.position.z);
+        //float x = timeElapsed;
+        self.position = new Vector3(self.position.x, y, self.position.z);
 
         if (timeElapsed > 1/frequency)
         {
